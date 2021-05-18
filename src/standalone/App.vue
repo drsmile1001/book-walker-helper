502c0b318fb1fdbc18d6ef51090359f2bfe412ba
drsmile <drsmile.1001@gmail.com>
2021-05-19 06:06:48 +0800

    feat: 調整版面 無資料集合不顯示標題

--- src/standalone/App.vue
+++ src/standalone/App.vue
@@ -1,51 +1,14 @@
 <template>
   <div class="p-4">
-    <h1>收藏</h1>
+    <h1>藏書</h1>
     <button v-if="!loading" @click="reloadCollection">重新讀取藏書</button>
     <div v-if="loading">{{ loadingProgress }}</div>
-    <h2>系列解析中</h2>
-    <div class="flex flex-wrap justify-center">
-      <div
-        class="flex p-4 items-center"
-        v-for="book in loadingSeries"
-        :key="book.id"
-      >
-        <img
-          :src="`https://image.bookwalker.com.tw/upload/product/${book.id}/${book.id}_4.jpg`"
-        />
-        <a
-          class="w-48 text-lg text-center"
-          target="_blank"
-          :href="`https://www.bookwalker.com.tw/browserViewer/${book.id}/read`"
-          >{{ book.name }}</a
-        >
-      </div>
-    </div>
-    <h2>非系列書</h2>
-    <div class="flex flex-wrap justify-center">
-      <div
-        class="flex p-4 items-center"
-        v-for="book in noSeries"
-        :key="book.id"
-      >
-        <img
-          :src="`https://image.bookwalker.com.tw/upload/product/${book.id}/${book.id}_4.jpg`"
-        />
-        <a
-          class="w-48 text-lg text-center"
-          target="_blank"
-          :href="`https://www.bookwalker.com.tw/browserViewer/${book.id}/read`"
-          >{{ book.name }}</a
-        >
-      </div>
-    </div>
-    <h2>系列書</h2>
-    <template v-for="series in seriesCollection" :key="series.name">
-      <h3>{{ series.name }}</h3>
-      <div class="flex flex-wrap justify-center">
+    <template v-if="loadingSeries.length">
+      <h2>尚未解析系列</h2>
+      <div class="flex flex-wrap">
         <div
           class="flex p-4 items-center"
-          v-for="book in series.books"
+          v-for="book in loadingSeries"
           :key="book.id"
         >
           <img
@@ -60,11 +23,54 @@
         </div>
       </div>
     </template>
+    <template v-if="noSeries.length">
+      <h2>非系列書</h2>
+      <div class="flex flex-wrap">
+        <div
+          class="flex p-4 items-center"
+          v-for="book in noSeries"
+          :key="book.id"
+        >
+          <img
+            :src="`https://image.bookwalker.com.tw/upload/product/${book.id}/${book.id}_4.jpg`"
+          />
+          <a
+            class="w-48 text-lg text-center"
+            target="_blank"
+            :href="`https://www.bookwalker.com.tw/browserViewer/${book.id}/read`"
+            >{{ book.name }}</a
+          >
+        </div>
+      </div>
+    </template>
+    <template v-if="seriesCollection.length">
+      <h2>系列書</h2>
+      <template v-for="series in seriesCollection" :key="series.name">
+        <h3>{{ series.name }}</h3>
+        <div class="flex flex-wrap">
+          <div
+            class="flex p-4 items-center"
+            v-for="book in series.books"
+            :key="book.id"
+          >
+            <img
+              :src="`https://image.bookwalker.com.tw/upload/product/${book.id}/${book.id}_4.jpg`"
+            />
+            <a
+              class="w-48 text-lg text-center"
+              target="_blank"
+              :href="`https://www.bookwalker.com.tw/browserViewer/${book.id}/read`"
+              >{{ book.name }}</a
+            >
+          </div>
+        </div>
+      </template>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive } from "vue"
+import { computed, defineComponent } from "vue"
 import {
   booksProperty,
   reloadCollection,
